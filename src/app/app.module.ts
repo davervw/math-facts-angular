d13c8bb2c1ec39ce8ce9be2ab85766f598e75c7c
Dave Van Wagner <git@davevw.com>
2022-10-01 11:40:46 -0700

    first implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { KeypadComponent } from './keypad/keypad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    KeypadComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [GameComponent]
 })
 export class AppModule { }
