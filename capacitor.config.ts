5601175a0fe79c76006ef6f31a07f8c8e1fe40f2
Dave Van Wagner <git@davevw.com>
2022-10-01 13:10:33 -0700

    capacitor/android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.davevw.mathfacts',
+  appName: 'math-facts',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
